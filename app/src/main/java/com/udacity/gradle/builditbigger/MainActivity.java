bcd38b4d207f7a308ddb206b97953ab17483fdb8
Andy <wirenite2@gmail.com>
2018-08-11 19:08:37 -0400

    at step 4, add functional tests

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,7 +1,9 @@
 package com.udacity.gradle.builditbigger;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -52,7 +54,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void tellJoke(View view) {
         String joke1 = joker1.getJoke();
-
+        new EndpointsAsyncTask().execute( new Pair<Context, String> (this
+                , joke1 ) );
 
     }
 
--- javaJokes/src/main/java/com/bkk/android/javajokes/Joker.java
+++ javaJokes/src/main/java/com/bkk/android/javajokes/Joker.java
@@ -1,9 +1,23 @@
 package com.bkk.android.javajokes;
 
+import java.util.List;
+import java.util.Random;
+
+import static java.util.Arrays.asList;
+
 public class Joker {
 
+    List<String> alltheJokes =  asList("joke1"
+            , "joke2", "joke3"
+            ,"joke4","joke5"
+            ,"joke6","joke7");
+
+
+
     public String getJoke() {
-        return "This is totally a funny joke";
+        // making a random number
+        int ranNum = new Random().nextInt(6);
+        return alltheJokes.get(ranNum);
     }
 
 }// class Joker

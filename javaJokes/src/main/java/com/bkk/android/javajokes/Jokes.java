eca9f67dd448be7ce73328115b868a62918d4970
Andy Murph <murph@email.com>
2018-08-14 10:52:40 -0400

    submission 3: remove the word Joke: from Jokes.java

--- javaJokes/src/main/java/com/bkk/android/javajokes/Jokes.java
+++ javaJokes/src/main/java/com/bkk/android/javajokes/Jokes.java
@@ -9,13 +9,13 @@ public class Jokes {
 
     // asList is an "immutable" list
     List<String> alltheJokes =  asList(
-            "Joke: What do you call the security outside of a Samsung Store?"
-            , "Joke: Why was the stadium so cold?"
-            , "Joke: What do you call a bee that was born is the United States?"
-            , "Joke: Why can't a bike stand up on it's own?"
-            , "Joke: What do we call a crying sister?"
-            , "Joke: Why was the king only a foot tall?"
-            , "Joke: What kind of music is a balloon scared of?"
+            "What do you call the security outside of a Samsung Store?"
+            , "Why was the stadium so cold?"
+            , "What do you call a bee that was born is the United States?"
+            , "Why can't a bike stand up on it's own?"
+            , "What do we call a crying sister?"
+            , "Why was the king only a foot tall?"
+            , "What kind of music is a balloon scared of?"
     );
 
 
